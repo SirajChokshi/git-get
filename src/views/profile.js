730010dcd73258bfbcba11f2c0167f82863d95f2
Davis Keene <dbkeene.tsyc@gmail.com>
2020-05-23 15:06:10 -0400

    update: github api keys

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,12 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import { Link, Redirect } from '@reach/router';
 
+const client_id = `Iv1.82a68fd3b00d5cab`
+const client_secret = `b6b9b2b580c0dfc25499d34d4e4fc9d4f33f0ce2`
+
 const Profile = (props) => {
 
     const [user, setUser] = useState()
 
     const fetchUser = (username) => {
-        fetch(`https://api.github.com/user/${username}`,
+        fetch(`https://api.github.com/users/${username}?client_id=${client_id}?client_secret=${client_secret}`,
             {method: "GET", headers: {'Content-Type': 'application/json'}}
         ).then(
             (userData => userData.json())
